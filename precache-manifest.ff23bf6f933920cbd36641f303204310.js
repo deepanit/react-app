451d119d5a7e3c8cae507db8acaba5b6e8cf0c47
deepanit <deepu2853@gmail.com>
2018-12-25 16:58:38 +0530

    Updates

---
+++ precache-manifest.ff23bf6f933920cbd36641f303204310.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "309a64d4320f48ad3c7a",
+    "url": "/react-app/static/css/main.5a82c33c.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7b07a5aac05feb63334c",
+    "url": "/react-app/static/js/1.7b07a5aa.chunk.js"
+  },
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "309a64d4320f48ad3c7a",
+    "url": "/react-app/static/js/main.309a64d4.chunk.js"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-app/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "7b07a5aac05feb63334c",
+    "url": "/react-app/static/css/1.08be5066.chunk.css"
+  },
+  {
+    "revision": "d5b4fc5f4c328c42885c2d7f212dde68",
+    "url": "/react-app/index.html"
+  }
+];
